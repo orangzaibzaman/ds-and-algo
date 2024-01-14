8496cb4a65a1fad155c4a7ca228c29ddb60e8a45
Orangzaib Zaman <orangzaib.zaman@capco.com>
2024-01-14 04:13:05 +0000

    First commit

---
+++ src/main/java/com/practice/dsandalgo/DsAndAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.dsandalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAndAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAndAlgoApplication.class, args);
+	}
+
+}
